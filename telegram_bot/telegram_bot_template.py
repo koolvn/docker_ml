54d1d469fdaeee73a3467b5eba26e5b91f09e7cc
Vladimir <kulyashovv@gmail.com>
2020-03-02 20:10:34 +0500

    Telegram bot

--- telegram_bot/telegram_bot_template.py
+++ telegram_bot/telegram_bot_template.py
@@ -11,8 +11,8 @@ WEBHOOK_HOST = '95.214.62.90'
 WEBHOOK_PORT = 8443  # 443, 80, 88 or 8443 (port need to be 'open')
 WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
-WEBHOOK_SSL_CERT = './certificates/webhook_cert.pem'  # Path to the ssl certificate
-WEBHOOK_SSL_PRIV = './certificates/webhook_private.key'  # Path to the ssl private key
+WEBHOOK_SSL_CERT = 'app/certificates/webhook_cert.pem'  # Path to the ssl certificate
+WEBHOOK_SSL_PRIV = 'app/certificates/webhook_private.key'  # Path to the ssl private key
 
 # Quick'n'dirty SSL certificate generation:
 #
