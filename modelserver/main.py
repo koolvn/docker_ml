49e63cb1c88601c34d02a8517d333d178602d4bd
Vladimir <kulyashovv@gmail.com>
2020-03-08 18:42:02 +0500

    added OpenCV face detection for image preprocessing
    Signed-off-by: Vladimir <kulyashovv@gmail.com>

--- modelserver/main.py
+++ modelserver/main.py
@@ -35,7 +35,21 @@ def base64_decode_image(a, dtype, shape):
     # Convert the string to a NumPy array using the supplied data
     # type and target shape
     a = np.frombuffer(base64.decodebytes(a), dtype=dtype)
-    a = a.reshape(shape)
+    # a = a.reshape(shape)
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # Read the input image
+    a = cv2.imdecode(a, cv2.IMREAD_COLOR)
+    # Convert into grayscale
+    gray = cv2.cvtColor(a, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 5)
+    # Draw rectangle around the faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(a, (x, y), (x+w, y+h), (255, 0, 0), 0)
+        # Save the output image
+        cv2.imwrite('detected.jpg', a[y:y+h, x:x+w])
+        a = a[y:y+h, x:x+w]
 
     # Return the decoded image
     return a
@@ -62,31 +76,13 @@ def classify_process():
         for q in queue:
             # Deserialize the object and obtain the input image
             q = json.loads(q.decode("utf-8"))
-            # Load the cascade
-            face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-            # Read the input image
-            nparr = np.fromstring(q['image'], np.uint8)
-            img = cv2.imdecode(nparr, cv2.IMREAD_COLOR) # cv2.IMREAD_COLOR in OpenCV 3.1
-            print(type(q['image']))
-            print(type(img))
-            # img = cv2.imread(q["image"])
 
-            # Convert into grayscale
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-            # Detect faces
-            faces = face_cascade.detectMultiScale(gray, 1.1, 5)
-            # Draw rectangle around the faces
-            for (x, y, w, h) in faces:
-                cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 0)
-                # Save the output image
-                cv2.imwrite('detected.jpg', img[y:y+h, x:x+w])
-                image = img[y:y+h, x:x+w]
-            # image = base64_decode_image(q["image"],
-            #                             os.environ.get("IMAGE_DTYPE"),
-            #                             (1, int(os.environ.get("IMAGE_HEIGHT")),
-            #                              int(os.environ.get("IMAGE_WIDTH")),
-            #                              int(os.environ.get("IMAGE_CHANS")))
-            #                             )
+            image = base64_decode_image(q["image"],
+                                        os.environ.get("IMAGE_DTYPE"),
+                                        (1, int(os.environ.get("IMAGE_HEIGHT")),
+                                         int(os.environ.get("IMAGE_WIDTH")),
+                                         int(os.environ.get("IMAGE_CHANS")))
+                                        )
 
             # Check to see if the batch list is None
             if batch is None:
